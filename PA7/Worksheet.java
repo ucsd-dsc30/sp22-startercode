2aec10b282c9028ab0b45be03743fb7741662a5a
Brian96086 <brian96086@gmail.com>
2022-05-09 23:36:05 -0700

    pushed worksheet startercode

---
+++ PA7/Worksheet.java
@@ -0,0 +1,53 @@
+/**
+ * PA7 Part 1 worksheet (style not required)
+ */
+public class Worksheet {
+
+    /**
+     * method that stores the result of Q1 in PA7 worksheet
+     * @return heap's array representation after insertions
+     */
+    public int[] insertionResult(){
+        /*
+        TODO: replace the values with the heap's array representations
+         */
+        int[] output =  new int[]{61, 20, 23, 80, 52, 69, 88, 57, 55, 66, 80, 95};
+        return output;
+    }
+
+    /**
+     * method that stores the result of Q2 in PA7 worksheet
+     * @return heap's array representations for each iteration of element removal, 5 iterations in total
+     */
+    public int[][] removalResult(){
+        /*
+        TODO: store the values with the heap's array representations for each removal iteration
+         */
+        int[][] output = new int[5][12];
+        return output;
+    }
+
+    /**
+     * method that stores the result of Q3 in PA7 worksheet
+     * @return heap's array representations for d-ary heaps, where d = 3,4
+     */
+    public int[][] dResult(){
+        /*
+        TODO: store 3-ary heap's array representation output[0] and store 4-ary representation in output[1]
+         */
+        int[][] output = new int[2][11];
+        return output;
+    }
+
+    /**
+     * method that stores the result of Q4 in PA7 worksheet
+     * @return array representations
+     */
+    public int[][] heapOperations(){
+        /*
+        TODO: store heap's array status after multiples operations - operation i will be stored at index i-1
+         */
+        int[][] output = new int[5][11];
+        return output;
+    }
+}
